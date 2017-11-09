4f94e68e02720be982553c83a129df920e6f867f
Nicolas Molina <nmolina@umewin.com>
2017-11-09 12:05:23 -0300

    Add polyfills for support

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,12 @@
 
 import React, { Component } from 'react';
 import { render } from 'react-dom';
-import 'intl';
 
+// Polyfills
+import 'intl';
+import 'intl/locale-data/jsonp/es.js';
+
+// Routes
 import Routes from './modules/Routes.jsx';
 
 render(
